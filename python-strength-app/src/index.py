d7e2c9176876497b1d5883434538e2e3b7cc3350
Niki Hietala <niki.hietala@gmail.com>
2022-04-05 22:20:17 +0300

    create index.py

---
+++ python-strength-app/src/index.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+from ui.ui import UI
+
+def main():
+    window = tk.Tk()
+    window.title("StrengthApp")
+
+    ui = UI(window)
+    ui.start()
+
+    window.mainloop()
+
+if __name__ == '__main__':
+    main()
